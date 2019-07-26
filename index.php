3066ed8751ff304bfa11cd6e2d40732f0ad691bb
Tiana Johnson <tiana.marie013@gmail.com>
2019-07-26 07:30:01 -0500

    list

--- index.php
+++ index.php
@@ -6,5 +6,10 @@
     <body>
         <title>PHP Practice Calculator</title>
         <p> learning to build with php </p>
+        <ul>
+            <li> how do I start? <li>
+            <li> what do I need to down load?<li>
+        <ul>
+
     </body>
 </html>
