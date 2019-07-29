688ebe38c7e10327762ce2574a2957366767e952
Tiana Johnson <tiana.marie013@gmail.com>
2019-07-28 20:24:44 -0500

    simple looop

--- index.php
+++ index.php
@@ -10,11 +10,18 @@
             <li> how do I start? </li>
             <li> what do I need to down load?</li>
         <ul>
+        </br>
     <?php
 
     $dog = "Ridgard";
-    echo "my dogs name is " . $dog;
+    echo "my dogs name is " . $dog . "! ";
 
+    $cute = true;
+
+    if ($cute == true) {
+        echo $dog . " is cute!";
+    } else { echo " you most not be talking about " . $dog;
+        }
     ?>
 
     </body>
