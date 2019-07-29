dc34baffd1597153eadf7fe2c2cefef89ce414f0
Tiana Johnson <tiana.marie013@gmail.com>
2019-07-28 20:15:13 -0500

    first php commit

--- index.php
+++ index.php
@@ -7,9 +7,15 @@
         <title>PHP Practice Calculator</title>
         <p> learning to build with php </p>
         <ul>
-            <li> how do I start? <li>
-            <li> what do I need to down load?<li>
+            <li> how do I start? </li>
+            <li> what do I need to down load?</li>
         <ul>
+    <?php
+
+    $dog = "Ridgard";
+    echo "my dogs name is " . $dog;
+
+    ?>
 
     </body>
 </html>
